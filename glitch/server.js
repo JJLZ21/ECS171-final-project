486dc3aceee60e425326f282ad73d02a39d4982a
NathanJWr <nathanielwright@protonmail.com>
2020-11-15 10:23:21 -0800

    python code can be run from a GET request

--- glitch/server.js
+++ glitch/server.js
@@ -1,12 +1,27 @@
 var express = require("express");
+const spawn = require("child_process").spawn;
+
 var app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('hello world!');
+    var dataToSend;
+
+    const pyprocess = spawn('python', ['./test.py']);
+
+    // Collect the data from the script
+    pyprocess.stdout.on('data', (data) => {
+	dataToSend = data.toString();
+	console.log(dataToSend);
+    });
+
+    pyprocess.on('close', (code) => {
+	console.log('py process finished with code ' + code);
+	res.send(dataToSend);
+    });
 });
 
 app.listen(port, () => {
-    console.log('Example app listening on port ${port}!')
+    console.log('Example app listening on port ' + port)
 });
 
