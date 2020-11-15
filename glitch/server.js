355e901aa820730115c97523a7614f331af9222f
NathanJWr <nathanielwright@protonmail.com>
2020-11-15 10:02:56 -0800

    added a basic expressjs server

---
+++ glitch/server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('hello world!');
+});
+
+app.listen(port, () => {
+    console.log('Example app listening on port ${port}!')
+});
+
