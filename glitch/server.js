7df78895831f9dc1c6241b9d198b9bd3550d4c13
Joel Boersma <joelboersma@gmail.com>
2020-11-17 17:21:10 -0800

    rename index.html to react.html

--- glitch/server.js
+++ glitch/server.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/react', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/react.html');
 });
 
 app.listen(port, () => {
