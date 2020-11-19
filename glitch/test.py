d5df3cf24c3d5dac5e5e845f4875edd9962bf829
JJLZ21 <59270294+JJLZ21@users.noreply.github.com>
2020-11-18 17:22:49 -0800

    save the model from hw1 and then use it to predict the mpg class testing

--- glitch/test.py
+++ glitch/test.py
@@ -1,1 +1,10 @@
+import pandas as pd
+import pickle
 print("Hello from Python!")
+
+# road the model from the python code and the run it
+filename = 'finalized_model.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
+input_for_weight = [16.75, 4, 400, 150, 8, 81, 1]
+load_model_predicted = loaded_model.predict([input_for_weight])
+print('The expected weight class is:', load_model_predicted[0])
