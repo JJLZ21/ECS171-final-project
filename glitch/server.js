850c857143bb11fe5d09b25d1bb3fa5df19ea507
NathanJWr <nathanielwright@protonmail.com>
2020-11-19 11:03:34 -0800

    changed test python to use pandas and print out json. To run example get request: localhost:3000/run-example

--- glitch/server.js
+++ glitch/server.js
@@ -27,6 +27,27 @@ app.get('/react', (req, res) => {
     res.sendFile(__dirname + '/public/react.html');
 });
 
+app.get('/run-example', (req, res) => {
+    // Run some example python code and send the results as json to the client
+    var dataToSend;
+
+    const pyprocess = spawn('python', ['./test.py']);
+
+    // Collect the data from the script
+    // Note: This data should already be in json format!!!
+    // This should be easy to accomplish and is probably better
+    // than trying to jsonify it here before sending it
+    pyprocess.stdout.on('data', (data) => {
+        dataToSend = data.toString();
+        console.log(dataToSend);
+    });
+
+    pyprocess.on('close', (code) => {
+        console.log('py process finished with code ' + code);
+        res.send(dataToSend);
+    });
+});
+
 app.listen(port, () => {
     console.log('Example app listening on port ' + port)
 });
--- glitch/test.py
+++ glitch/test.py
@@ -1,1 +1,8 @@
-print("Hello from Python!")
+import pandas as pd
+
+data = {'Test Column 1': ['First Val', 'Second Val'],
+        'Test Column 2': ['Second Val', 'Second Val']}
+
+df = pd.DataFrame(data , columns = ['Test Column 1', 'Test Column 2']);
+
+print(df.to_json())
