6f737180e24043f07974861dac4a38a82fdc6b83
Joel Boersma <joelboersma@gmail.com>
2020-11-20 23:10:29 -0800

    react-plotly.js example code

--- glitch/src/components/PlottingTest.jsx
+++ glitch/src/components/PlottingTest.jsx
@@ -1,8 +1,21 @@
-const React = require('react')
+const React = require('react');
+const Plot = require('react-plotly.js');
 
 const PlottingTest = function() {
    return (
-      <p>"Plotting test"</p>
+      <Plot
+         data={[
+            {
+               x: [1, 2, 3],
+               y: [2, 6, 3],
+               type: 'scatter',
+               mode: 'lines+markers',
+               marker: {color: 'red'},
+            },
+            {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+         ]}
+         layout={{width: 320, height: 240, title: 'A Fancy Plot'}}
+      />
    );
 }
 
