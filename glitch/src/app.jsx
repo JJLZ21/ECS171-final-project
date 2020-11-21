ccd9794d435a03bfdc4ecc1a906ddeff2acb86d0
Joel Boersma <joelboersma@gmail.com>
2020-11-21 13:03:32 -0800

    PlottingTest is gone let's goooo

--- glitch/src/app.jsx
+++ glitch/src/app.jsx
@@ -5,14 +5,14 @@ const ReactDOM = require('react-dom');
 
 /* Import Components */
 const HelloWorld = require('./components/HelloWorld');
-const PlottingTest = require('./components/PlottingTest');
+// const PlottingTest = require('./components/PlottingTest');
 
 // Check whether to render plotting-test page
 const pltPath = "plotting-test"
 const url = window.location.href;
 const sub = url.slice(url.length - pltPath.length, url.length);
 if (sub == pltPath) {
-   ReactDOM.render(<PlottingTest/>, document.getElementById('main'));
+   // ReactDOM.render(<PlottingTest/>, document.getElementById('main'));
 }
 else {
    ReactDOM.render(<HelloWorld/>, document.getElementById('main'));
