8330ee0a9191b830884fce1116ac9d4fa61c9b01
Troi-Ryan S <troiryans@gmail.com>
2020-11-21 13:45:41 -0800

    add a skeleton for the results page

--- glitch/src/app.jsx
+++ glitch/src/app.jsx
@@ -3,17 +3,15 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-/* Import Components */
+/* Import Components (Pages) */
 const HelloWorld = require('./components/HelloWorld');
-// const PlottingTest = require('./components/PlottingTest');
+const Results = require('./components/Results');
 
 // Check whether to render plotting-test page
-const pltPath = "plotting-test"
 const url = window.location.href;
-const sub = url.slice(url.length - pltPath.length, url.length);
-if (sub == pltPath) {
-   // ReactDOM.render(<PlottingTest/>, document.getElementById('main'));
-}
-else {
-   ReactDOM.render(<HelloWorld/>, document.getElementById('main'));
+const sub = url.slice(url.lastIndexOf("/") + 1);
+
+switch (sub) {
+    case "results": ReactDOM.render(<Results/>, document.getElementById('main')); break;
+    default: ReactDOM.render(<HelloWorld/>, document.getElementById('main'));
 }
---
+++ glitch/src/components/Results.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+
+const di = require('./DataInterface');
+
+const Results = function() {
+    return (
+      <div>
+        <h1>Results Page</h1>
+      </div>
+    );
+  }
+  
+  module.exports = Results;
