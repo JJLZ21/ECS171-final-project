899f038598d177bc9a98427da870674b730afc20
Troi-Ryan S <troiryans@gmail.com>
2020-11-22 00:34:37 -0800

    use a json file to allow easy creation of results

--- glitch/src/components/Results.jsx
+++ glitch/src/components/Results.jsx
@@ -2,44 +2,69 @@ const React = require('react');
 
 const di = require('./DataInterface');
 
-class TestRequest extends React.Component {
+// require the categories data json
+const results_data = require('./categories.json')
+
+class ResultBody extends React.Component {
 
     constructor(props) {
         super(props);
 
-        // set the state to just have the request result
+        // set the state to just have the category number
         this.state = {
-            requestResult: "None"
+            categoryNumber: -1
         };
     }
 
     // after the component is successfully mounted, create a GET request to the python server
-    // change the requestResult to whether it was succesfully received or not
-    // **use json to read data from the server, see the above link
+    // change the category number according to what is received
     componentDidMount() {
         // NON BLOCkING
         di.requestData((data) => {
             let columns = [...data.keys()];
             this.setState({
-                requestResult: columns[0] + ": " + data.get(columns[0])
+                categoryNumber: data.get(columns[0])
             });
         });
     }
 
     // function of what to render onto the DOM
     render() {
-        const { requestResult } = this.state;
-        return requestResult;
+        // read in the current state
+        const { categoryNumber } = this.state;
+
+        // get all of the categories in the json file
+        let categories = Object.keys(results_data);
+        
+        // loop through all of those categories
+        for (let i = 0; i < categories.length; i++) {
+            let categoryName = categories[i];
+
+            // if the category_number matches the current state
+            if (results_data[categoryName].category_number == categoryNumber) {
+                // return the html body for that category according to what is in the json
+                return (
+                    <div>
+                        <h2>{categoryName}</h2>
+                        <p>{results_data[categoryName].body.map(function (str) {
+                            return <div>{str}<br /></div>;
+                        })}</p>
+                    </div>
+                );
+            }
+        }
+
+        // there was no known categories in the json file
+        return "Unknown Category";
     }
 }
 
-const Results = function() {
+const ResultComponent = function() {
     return (
       <div>
-        <h1>Results Page</h1>
-        <p><TestRequest /></p>
+        <p><ResultBody /></p>
       </div>
     );
   }
   
-  module.exports = Results;
+  module.exports = ResultComponent;
