94cec352a586c6a0bbfd4c685e369e159dd059d7
Joel Boersma <joelboersma@gmail.com>
2020-11-21 22:32:51 -0800

    Router defaults to questions page

--- glitch/src/app.jsx
+++ glitch/src/app.jsx
@@ -5,12 +5,12 @@ const ReactDOM = require('react-dom');
 
 /* Import Components (Pages) */
 const HelloWorld = require('./components/HelloWorld');
+const Questions = require('./components/Questions');
 const Results = require('./components/Results');
 
-// Check whether to render plotting-test page
+// Choose which page to render
 const url = window.location.href;
 const sub = url.slice(url.lastIndexOf("/") + 1);
-
 switch (sub) {
     case "results": ReactDOM.render(<Results/>, document.getElementById('main')); break;
     default: ReactDOM.render(<HelloWorld/>, document.getElementById('main'));
