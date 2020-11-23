be70f95b905135c659b0801f06b21ffcfb5b3eae
Joel Boersma <joelboersma@gmail.com>
2020-11-23 12:37:39 -0800

    Questions page has working form

--- glitch/src/components/Questions.jsx
+++ glitch/src/components/Questions.jsx
@@ -1,8 +1,79 @@
 const React = require('react');
 
+class FormItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    render() {
+        return (
+            <label>
+                {this.props.myLabel}
+                <input type='text' value={this.state.value} onChange={this.handleChange} />
+            </label>
+        );
+    }
+}
+
+class QuestionsForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+        this.FormItem1 = React.createRef();
+        this.FormItem2 = React.createRef();
+        this.FormItem3 = React.createRef();
+        this.FormItem4 = React.createRef();
+        this.FormItem5 = React.createRef();
+    }
+
+    handleSubmit(event) {
+        console.log('Response submitted:', 
+            this.FormItem1.current.state.value,
+            this.FormItem2.current.state.value,
+            this.FormItem3.current.state.value,
+            this.FormItem4.current.state.value,
+            this.FormItem5.current.state.value
+        );
+        event.preventDefault();
+    }
+
+    render() {
+        const labels = {
+            label1: 'Question 1',
+            label2: 'Question 2',
+            label3: 'Question 3',
+            label4: 'Question 4',
+            label5: 'Question 5'
+        }
+
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <FormItem myLabel={labels.label1} ref={this.FormItem1} />
+                <FormItem myLabel={labels.label2} ref={this.FormItem2} />
+                <FormItem myLabel={labels.label3} ref={this.FormItem3} />
+                <FormItem myLabel={labels.label4} ref={this.FormItem4} />
+                <FormItem myLabel={labels.label5} ref={this.FormItem5} />
+                <input type='submit' value='Submit' />
+            </form>
+        );
+    }
+}
+
 const Questions = function() {
     return(
-        <h1>Questions Page</h1>
+        <div>
+            <h1>Questions Page</h1>
+            <QuestionsForm/>
+        </div>
     );
 }
 
