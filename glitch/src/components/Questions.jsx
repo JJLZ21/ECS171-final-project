813842c8dbac926cd91b2ea40af41e9a251d3b4d
Joel Boersma <joelboersma@gmail.com>
2020-11-25 21:38:42 -0800

    Implemented questions given by Mary on Slack

--- glitch/src/components/Questions.jsx
+++ glitch/src/components/Questions.jsx
@@ -1,15 +1,12 @@
 const React = require('react');
 const { pushData } = require('./DataInterface');
 
-// Once we know our actual questions, place them here.
-// If we have less than five questions, remove necessary code.
-// If we have more than five questions, add necessary code.
+// Questions given by Mary in #se_and_ui-ux channel in Slack
+// Subject to change
 const labels = {
-    label1: 'Question 1',
-    label2: 'Question 2',
-    label3: 'Question 3',
-    label4: 'Question 4',
-    label5: 'Question 5'
+    label1: 'When was the last time this client shopped at your store?',
+    label2: 'How much has this customer spent in total? (£)',
+    label3: 'How often does this customer make a purchase?'
 }
 
 class FormItem extends React.Component {
@@ -43,22 +40,16 @@ class QuestionsForm extends React.Component {
         this.FormItem1 = React.createRef();
         this.FormItem2 = React.createRef();
         this.FormItem3 = React.createRef();
-        this.FormItem4 = React.createRef();
-        this.FormItem5 = React.createRef();
     }
 
     handleSubmit(event) {
         event.preventDefault();
         const responsesJSON = {
-            responses: [
-                this.FormItem1.current.state.value,
-                this.FormItem2.current.state.value,
-                this.FormItem3.current.state.value,
-                this.FormItem4.current.state.value,
-                this.FormItem5.current.state.value
-            ]
+            // Answers to our three questions
+            recencyIn: this.FormItem1.current.state.value,
+            monetaryIn: this.FormItem2.current.state.value,
+            frequencyIn: this.FormItem3.current.state.value,
         };
-        console.log(responsesJSON);
 
         // Send to server (through our DataInterface)
         pushData(responsesJSON);
@@ -70,8 +61,6 @@ class QuestionsForm extends React.Component {
                 <FormItem myLabel={labels.label1} ref={this.FormItem1} />
                 <FormItem myLabel={labels.label2} ref={this.FormItem2} />
                 <FormItem myLabel={labels.label3} ref={this.FormItem3} />
-                <FormItem myLabel={labels.label4} ref={this.FormItem4} />
-                <FormItem myLabel={labels.label5} ref={this.FormItem5} />
                 <input type='submit' value='Submit' />
             </form>
         );
