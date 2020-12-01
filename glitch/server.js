35c621b7597bac31a8acc94cb47c483572179fc9
NathanJWr <nathanielwright@protonmail.com>
2020-12-01 09:45:35 -0800

    made more things const

--- glitch/server.js
+++ glitch/server.js
@@ -22,67 +22,65 @@ function distFromMean(input, mean, std) {
 }
 
 function categoryZeroAnalysis(frequency, monetary, recency) {
-    let mean_r = 17.23;
-    let mean_f = 213.11;
-    let mean_m = 4435.33;
+    const mean_r = 17.23;
+    const mean_f = 213.11;
+    const mean_m = 4435.33;
 
-    let std_r = 14.011974;
-    let std_f = 288.561883;
-    let std_m = 12952.823168;
+    const std_r = 14.011974;
+    const std_f = 288.561883;
+    const std_m = 12952.823168;
 
-    let zr = distFromMean(recency, mean_r, std_r);
-    let zf = distFromMean(frequency, mean_f, std_f);
-    let zm = distFromMean(monetary, mean_m, std_m);
+    const zr = distFromMean(recency, mean_r, std_r);
+    const zf = distFromMean(frequency, mean_f, std_f);
+    const zm = distFromMean(monetary, mean_m, std_m);
 
     return [zf, zm, zr];
 }
 
 function categoryOneAnalysis(frequency, monetary, recency) {
-    // Mean:     R = 24.34, F = 29.92, M = 497.39
-    let mean_r = 24.34;
-    let mean_f = 29.92;
-    let mean_m = 497.39;
+    const mean_r = 24.34;
+    const mean_f = 29.92;
+    const mean_m = 497.39;
 
-    let std_r = 14.168621;
-    let std_f = 22.185703;
-    let std_m = 440.082541;
+    const std_r = 14.168621;
+    const std_f = 22.185703;
+    const std_m = 440.082541;
 
-    let zr = distFromMean(recency, mean_r, std_r);
-    let zf = distFromMean(frequency, mean_f, std_f);
-    let zm = distFromMean(monetary, mean_m, std_m);
+    const zr = distFromMean(recency, mean_r, std_r);
+    const zf = distFromMean(frequency, mean_f, std_f);
+    const zm = distFromMean(monetary, mean_m, std_m);
 
     return [zf, zm, zr];
 }
 
 function categoryTwoAnalysis(frequency, monetary, recency) {
-// Mean:  R = 185.89, F = 18.70, M = 300.99
-    let mean_r = 185.89;
-    let mean_f = 18.70;
-    let mean_m = 300.99;
+    const mean_r = 185.89;
+    const mean_f = 18.70;
+    const mean_m = 300.99;
 
-    let std_r = 97.394688;
-    let std_f = 13.746029;
-    let std_m = 206.843209;
+    const std_r = 97.394688;
+    const std_f = 13.746029;
+    const std_m = 206.843209;
 
-    let zr = distFromMean(recency, mean_r, std_r);
-    let zf = distFromMean(frequency, mean_f, std_f);
-    let zm = distFromMean(monetary, mean_m, std_m);
+    const zr = distFromMean(recency, mean_r, std_r);
+    const zf = distFromMean(frequency, mean_f, std_f);
+    const zm = distFromMean(monetary, mean_m, std_m);
 
     return [zf, zm, zr];
 }
 
 function categoryThreeAnalysis(frequency, monetary, recency ) {
-    let mean_r = 118.59;
-    let mean_f = 94.41;
-    let mean_m = 1770.96;
+    const mean_r = 118.59;
+    const mean_f = 94.41;
+    const mean_m = 1770.96;
 
-    let std_r = 70.645585;
-    let std_f = 93.843974;
-    let std_m = 2053.940432;
+    const std_r = 70.645585;
+    const std_f = 93.843974;
+    const std_m = 2053.940432;
 
-    let zr = distFromMean(recency, mean_r, std_r);
-    let zf = distFromMean(frequency, mean_f, std_f);
-    let zm = distFromMean(monetary, mean_m, std_m);
+    const zr = distFromMean(recency, mean_r, std_r);
+    const zf = distFromMean(frequency, mean_f, std_f);
+    const zm = distFromMean(monetary, mean_m, std_m);
 
     return [zf, zm, zr];
 }
